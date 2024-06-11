732e759b7eaf08efed825ebc10bf2c25632b418b
username <useremail>
2024-06-11 16:14:00 -0700

    Country

--- Country.py
+++ Country.py
@@ -4,10 +4,10 @@
 
 def display_menu():
     print("COMMAND MENU")
-    print("view --> View country name")
-    print("add --> Add a country")
-    print("delete --> Delete a country")
-    print("exit --> Exit the program")
+    print("view - View country name")
+    print("add - Add a country")
+    print("del - Delete a country")
+    print("exit - Exit program")
     print()
     
 def display_codes(countries):   
@@ -19,35 +19,35 @@ def display_codes(countries):
     print(codes_line)
     
 def view(countries):
-    display_codes("Enter country code:  ")
+    display_codes(countries)
+    code = input("Enter country code: ")
     code = code.upper()
     if code in countries:
         name = countries[code]
-        print(f"Country name: {name}. \n")
+        print(f"Country name: {name}.\n")
     else:
-        print("There is no country with that code. \n")
+        print("There is no country with that code.\n")
         
 def add(countries):
     code = input("Enter country code: ")
     code = code.upper()
     if code in countries:
         name = countries[code]
-        print(f"{name} is already using that code. \n")
-        
+        print(f"{name} is already using that code.\n")
     else:
         name = input("Enter country name: ")
         name = name.title()
         countries[code] = name
-        print(f"{name} was added. \n")
+        print(f"{name} was added.\n")
         
 def delete(countries):
     code = input("Enter country code: ")
     code = code.upper()
     if code in countries:
         name = countries.pop(code)
-        print(f"{name} was deleted. \n")
+        print(f"{name} was deleted.\n")
     else:
-        print("There is no country with that code. \n")
+        print("There is no country with that code.\n")
         
 def main():
     countries = {"AF": "Afghanistan", "DE": "Germany", "US": "United States"}
@@ -60,13 +60,13 @@ def main():
             view(countries)
         elif command == "add":
             add(countries)
-        elif command == "delete":
+        elif command == "del":
             delete(countries)
         elif command == "exit":
-            print("Bye")
+            print("Bye!")
             break
         else:
-            print("Not a valid command, please try again. \n")
+            print("Not a valid command, please try again.\n")
             
 if __name__ == "__main__":
     main()
